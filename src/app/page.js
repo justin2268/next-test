8a75445e620accf1e8ff4f7f6ab093fbd9a6380d
Justin Moriarty <75666702+justin2268@users.noreply.github.com>
2024-06-14 02:13:38 +0000

    added dynamic route

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
   return (
     <div>
       <div>
         <h1>Enter Your Name</h1>
         </div>
-        <form>
-          <input type="text" placeholder="Type your name..." />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Type your name..."
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}/>
           <button type="submit"> Predict Data</button> 
         </form>
     </div>
